097d42e31f763bc2ef2afab56ca58432dc4279ed
hunter-sn <63088281+hunter-sn@users.noreply.github.com>
2026-03-09 22:16:20 +0800

    Update build.gradle.kts

--- simple_live_app/android/app/build.gradle.kts
+++ simple_live_app/android/app/build.gradle.kts
@@ -40,21 +40,21 @@ android {
     }
 
     signingConfigs {
-        create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
-            storePassword = keystoreProperties["storePassword"] as String
-            isV1SigningEnabled = true
-            isV2SigningEnabled = true
-        }
+       // create("release") {
+       //     keyAlias = keystoreProperties["keyAlias"] as String
+       //     keyPassword = keystoreProperties["keyPassword"] as String
+       //     storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+       //     storePassword = keystoreProperties["storePassword"] as String
+       //     isV1SigningEnabled = true
+       //     isV2SigningEnabled = true
+       // }
     }
 
     buildTypes {
         release {
             // Signing with the debug keys for now, so `flutter run --release` works.
             // `release` or `debug`
-            signingConfig = signingConfigs.getByName("release")
+            signingConfig = signingConfigs.getByName("debug")
             isMinifyEnabled = true
             isShrinkResources = true
             proguardFiles(
