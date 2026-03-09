c71d40ae5f393add6e7462cba361eedb27a71548
hunter-sn <63088281+hunter-sn@users.noreply.github.com>
2026-03-09 21:35:28 +0800

    Update build.gradle.kts
    修改 signingConfig = signingConfigs.getByName("release")   解决编译使用个签问题

--- simple_live_app/android/app/build.gradle.kts
+++ simple_live_app/android/app/build.gradle.kts
@@ -53,7 +53,8 @@ android {
     buildTypes {
         release {
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("release")
+            //signingConfig = signingConfigs.getByName("release")
+            signingConfig = signingConfigs.getByName("debug")
             isMinifyEnabled = true
             isShrinkResources = true
             proguardFiles(
